fda936c755ba815f91bf8fe917ee01b7cbe112d7
David Runge <dave@sleepmap.de>
2019-04-25 20:24:09 +0200

    conf.py: Adding default deploy command.

--- conf.py
+++ conf.py
@@ -624,11 +624,11 @@ REDIRECTIONS = []
 # to `nikola deploy`.  If no arguments are specified, a preset
 # named `default` will be executed.  You can use as many presets
 # in a `nikola deploy` command as you like.
-# DEPLOY_COMMANDS = {
-#     'default': [
-#         "rsync -rav --delete output/ joe@my.site:/srv/www/site",
-#     ]
-# }
+DEPLOY_COMMANDS = {
+    'default': [
+        "rsync -rav --delete output/ eoc@eo-charlottenburg.de:/srv/http/websites/eo-charlottenburg.de",
+    ]
+}
 
 # github_deploy configuration
 # For more details, read the manual:
