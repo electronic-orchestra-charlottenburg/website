749542484991e000a95c9ce228b0f5fe9bf8ca8f
David Runge <dave@sleepmap.de>
2019-04-27 11:22:53 +0200

    conf.py: Switching to theme cadair.

--- conf.py
+++ conf.py
@@ -153,7 +153,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap4-jinja"
+THEME = "cadair"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
